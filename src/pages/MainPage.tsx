697d01c95aaed28c9ba4be2033ea569d67c0a593
leeyounggyun <noxium@naver.com>
2022-10-13 19:38:30 +0900

    MainPage

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <a>
+            <span>Hello</span>
+          </a>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>calculator</a>
+          </li>
+          <li>
+            <a>timer</a>
+          </li>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default MainPage;
