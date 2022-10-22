3571bade92271c0003baaf0804901be353d7d305
leeyounggyun <noxium@naver.com>
2022-10-23 01:34:04 +0900

    calculator

--- src/component/Buttons.tsx
+++ src/component/Buttons.tsx
@@ -1,27 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Buttons(): React.ReactElement {
+function Buttons(props: { changevalue: (arg0: string) => void; }): React.ReactElement {
+   const [btnValue, setBtnValue] = useState("0")
+;
+  const handleButtonPress = (content: any) => () => {
+      setBtnValue((parseFloat(btnValue) + content).toString);
+      props.changevalue(btnValue)
+  };
   return (
     <div className="grid gap-3 grid-rows-5 grid-cols-4">
-      <button className="btn-function">AC</button>
-      <button className="btn-function">±</button>
-      <button className="btn-function">%</button>
-      <button className="btn-operator">÷</button>
-      <button className="btn-calculator">7</button>
-      <button className="btn-calculator">8</button>
-      <button className="btn-calculator">9</button>
-      <button className="btn-operator">x</button>
-      <button className="btn-calculator">4</button>
-      <button className="btn-calculator">5</button>
-      <button className="btn-calculator">6</button>
-      <button className="btn-operator">-</button>
-      <button className="btn-calculator">1</button>
-      <button className="btn-calculator">2</button>
-      <button className="btn-calculator">3</button>
-      <button className="btn-operator">+</button>
-      <button className="btn-zero">0</button>
-      <button className="btn-calculator">.</button>
-      <button className="btn-operator">=</button>
+      <button onClick={handleButtonPress} className="btn-function">
+        AC
+      </button>
+      <button onClick={handleButtonPress} className="btn-function">
+        ±
+      </button>
+      <button onClick={handleButtonPress} className="btn-function">
+        %
+      </button>
+      <button onClick={handleButtonPress} className="btn-operator">
+        ÷
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        7
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        8
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        9
+      </button>
+      <button onClick={handleButtonPress} className="btn-operator">
+        x
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        4
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        5
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        6
+      </button>
+      <button onClick={handleButtonPress} className="btn-operator">
+        -
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        1
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        2
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        3
+      </button>
+      <button onClick={handleButtonPress} className="btn-operator">
+        +
+      </button>
+      <button onClick={handleButtonPress} className="btn-zero">
+        0
+      </button>
+      <button onClick={handleButtonPress} className="btn-calculator">
+        .
+      </button>
+      <button onClick={handleButtonPress} className="btn-operator">
+        =
+      </button>
     </div>
   );
 }
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Buttons from "../component/Buttons";
 
 function Calculator() {
-  const [value, setValue] = useState("0");
+    const [value, setValue] = useState("0");
 
-  return (
+    return (
     <div className="w-full h-full bg-black text-white rounded-3xl p-5 box-border">
       <div className="h-64 px-5 py-0">4:43</div>
-      <div className="text-9xl mb-5 font-light text-right">0</div>
-      <Buttons />
+      <div className="text-9xl mb-5 font-light text-right">{value}</div>
+      <Buttons
+          changevalue={value => setValue(value)}/>
       <div className="bottom">-</div>
     </div>
   );
