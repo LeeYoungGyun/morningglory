1184f69be70777287b81ef4df3f436b8375f129f
leeyounggyun <noxium@naver.com>
2022-10-24 00:31:26 +0900

    calculator

--- src/component/Buttons.tsx
+++ src/component/Buttons.tsx
@@ -1,72 +1,80 @@
-import React, {useState} from "react";
+import React, {MouseEvent, Dispatch, ReactElement, SetStateAction,} from "react";
 
-function Buttons(props: { changevalue: (arg0: string) => void; }): React.ReactElement {
-   const [btnValue, setBtnValue] = useState("0")
-;
-  const handleButtonPress = (content: any) => () => {
-      setBtnValue((parseFloat(btnValue) + content).toString);
-      props.changevalue(btnValue)
+function Buttons(props: {
+  value: string;
+  changeValue: Dispatch<SetStateAction<string>>;
+}): ReactElement {
+  const { value, changeValue } = props;
+
+  const handleButtonPress = (e: MouseEvent<HTMLButtonElement>) => {
+    if (value === "0") {
+      changeValue(e.currentTarget.value);
+    } else {
+      changeValue(value + e.currentTarget.value);
+    }
   };
+
   return (
     <div className="grid gap-3 grid-rows-5 grid-cols-4">
       <button onClick={handleButtonPress} className="btn-function">
         AC
       </button>
-      <button onClick={handleButtonPress} className="btn-function">
+      <button onClick={handleButtonPress} value="±" className="btn-function">
         ±
       </button>
-      <button onClick={handleButtonPress} className="btn-function">
+      <button onClick={handleButtonPress} value="%" className="btn-function">
         %
       </button>
-      <button onClick={handleButtonPress} className="btn-operator">
+      <button onClick={handleButtonPress} value="÷" className="btn-operator">
         ÷
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="7" className="btn-calculator">
         7
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="8" className="btn-calculator">
         8
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="9" className="btn-calculator">
         9
       </button>
-      <button onClick={handleButtonPress} className="btn-operator">
+      <button onClick={handleButtonPress} value="x" className="btn-operator">
         x
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="4" className="btn-calculator">
         4
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="5" className="btn-calculator">
         5
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="6" className="btn-calculator">
         6
       </button>
-      <button onClick={handleButtonPress} className="btn-operator">
+      <button onClick={handleButtonPress} value="-" className="btn-operator">
         -
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="1" className="btn-calculator">
         1
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="2" className="btn-calculator">
         2
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="3" className="btn-calculator">
         3
       </button>
-      <button onClick={handleButtonPress} className="btn-operator">
+      <button onClick={handleButtonPress} value="+" className="btn-operator">
         +
       </button>
-      <button onClick={handleButtonPress} className="btn-zero">
+      <button onClick={handleButtonPress} value="0" className="btn-zero">
         0
       </button>
-      <button onClick={handleButtonPress} className="btn-calculator">
+      <button onClick={handleButtonPress} value="." className="btn-calculator">
         .
       </button>
-      <button onClick={handleButtonPress} className="btn-operator">
+      <button onClick={handleButtonPress} value="=" className="btn-operator">
         =
       </button>
     </div>
   );
 }
+
 export default Buttons;
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Buttons from "../component/Buttons";
 
 function Calculator() {
-    const [value, setValue] = useState("0");
+  const [value, setValue] = useState("0");
+  console.log("LLLLLL" + value);
 
-    return (
+  return (
     <div className="w-full h-full bg-black text-white rounded-3xl p-5 box-border">
       <div className="h-64 px-5 py-0">4:43</div>
       <div className="text-9xl mb-5 font-light text-right">{value}</div>
-      <Buttons
-          changevalue={value => setValue(value)}/>
+      <Buttons value={value} changeValue={setValue} />
       <div className="bottom">-</div>
     </div>
   );
