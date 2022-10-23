19f72938a148488f01d14f6cd0271c160e672700
YoungGyun <noxium@naver.com>
2022-10-24 01:52:10 +0900

    modify css

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -6,11 +6,13 @@ function Calculator() {
   console.log("LLLLLL" + value);
 
   return (
-    <div className="w-full h-full bg-black text-white rounded-3xl p-5 box-border">
-      <div className="h-64 px-5 py-0">4:43</div>
-      <div className="text-9xl mb-5 font-light text-right">{value}</div>
-      <Buttons value={value} changeValue={setValue} />
-      <div className="bottom">-</div>
+    <div className="flex justify-center">
+      <div className="w-full h-full bg-black text-white rounded-3xl p-5 box-border max-w-lg">
+        <div className="h-64 px-5 py-0">4:43</div>
+        <div className="text-9xl mb-5 font-light text-right">{value}</div>
+        <Buttons value={value} changeValue={setValue} />
+        <div className="bottom">-</div>
+      </div>
     </div>
   );
 }
