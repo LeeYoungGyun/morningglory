66b3fc9b8c60fec9d972c4709687d2f967a394aa
YoungGyun <noxium@naver.com>
2022-11-02 16:48:11 +0900

    MainPage CSS

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
-    <div >
+    <div>
       <nav>
-        <div className="toolbar">
+        <div className="bg-amber-50 leading-5 px-2">
           <a>
-            <span>Hello</span>
+            <span className="text-blue-500 text-4xl leading-normal">
+              morningglory
+            </span>
           </a>
-          <li>
+          <ul className="float-right pt-1.5">
+          <li className="float-left list-none px-5">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="float-left list-none px-5">
             <Link to="pages/calculator">Calculator</Link>
           </li>
-          <li>
+          <li className="float-left list-none pr-96">
             <a>timer</a>
           </li>
+          </ul>
         </div>
       </nav>
     </div>
