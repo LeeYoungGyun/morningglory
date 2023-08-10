a7704058f16e3249e1a95e66258d2227655d2f28
Younggyun <noxium@naver.com>
2023-08-10 14:27:48 +0900

    Ladder Game5

--- src/component/LadderGame.tsx
+++ src/component/LadderGame.tsx
@@ -1,7 +1,7 @@
 import React, {  } from "react";
 import Navigation from "../component/Navigation";
 import { useSelector, useDispatch } from 'react-redux';
-import { setParticipant } from '../actions/participantActions';
+// import { setParticipant } from '../actions/participantActions';
 import { RootState } from '../store/store';
 
 function LadderGame() {
@@ -10,17 +10,33 @@ function LadderGame() {
 
   console.log('participantState====', participant)
 
+  // const test = () => {
+  //   const rrrr = "younggyun"
+  //   const url = `/admin/detail/${rrrr}`;
+  //   console.log('rrrr====',rrrr);
+
+  //   console.log('url====', url);
+  // };
+
   return (
     <>
       <Navigation />
-      <div className="flex mt-16">
+      <div className="flex justify-center items-center mt-16 center">
         {/* <!-- Repeat the below block for each "vertical" line in the ladder --> */}
-        <div className="relative border-l-2 border-black w-4 h-64 mx-10">
-            <input type="text" className="absolute top-[-30px] left-[-20px] border rounded-3xl p-1" />
-            <div className="absolute left-[-2px] top-[10%] border-t-2 border-black w-[80px]"></div>
-            <input type="text" className="absolute bottom-[-30px] left-[-20px] border rounded-3xl p-1" />
+        {Array.from({ length: participant }).map((_, index) => (
+        <div key={index} className="relative border-l-2 border-black w-8 h-64 mx-16">
+          <input type="text" className="absolute top-[-30px] left-[-70px] border rounded-3xl p-1 text-center w-36" />
+          <div className="absolute left-[-2px] top-[10%] border-t-2 border-black"></div>
+          <input type="text" className="absolute bottom-[-30px] left-[-70px] border rounded-3xl p-1 text-center w-36" />
         </div>
-    </div>
+      ))}
+      </div>
+      <div className="flex justify-center mt-20">
+        {/* <button className="border-black border-2 w-14">Back</button> */}
+        <button className="button button-first">Back</button>
+        <button className="button button-second ml-11">Start</button>
+        {/* <button className="border-black border-2 w-14 ml-11">Start</button> */}
+      </div>
     </>
   );
 }
